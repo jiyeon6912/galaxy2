145b9156946d0a38e5e834f092a5a94a77728d6c
jiyeon6912 <jiyeon6912@gmail.com>
2020-11-03 15:45:33 +0900

    galaxy_2nd

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const slide_btn = document.querySelector('.slide_btn')
 const slide_modal = document.querySelector('.slide_modal')
 const close = document.querySelector('.close')
+const btn_white = document.querySelector('.btn_white')
+const banner_modal = document.querySelector('.banner_modal')
+const banner_close = document.querySelector('.banner_close')
 
 slide_btn.addEventListener('click', function() {
-  console.log("hahaha")
+  console.log("slide_btn")
   slide_modal.classList.add('open')
 })
 
-close.addEventListener('click',function(){
+close.addEventListener('click',function() {
+  console.log("close")
   slide_modal.classList.remove('open')
+})
+
+btn_white.addEventListener('click',function(){
+  console.log('btn_white')
+  banner_modal.classList.add('open')
+})
+
+banner_close.addEventListener('click',function(){
+  console.log('banner_close')
+  banner_modal.classList.remove('open')
 })
